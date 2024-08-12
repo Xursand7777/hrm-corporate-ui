05fae5eeb51bfca07f9accead081afc9f47695bf
Hamid-Solih <solihovhamid@gmail.com>
2024-08-12 11:58:20 +0500

    chore: add tailwind

---
+++ hrm-corporate-ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './projects/ngx-hrm-corporate-ui/src/**/*.{html,ts}' ],
+    theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+}
+
